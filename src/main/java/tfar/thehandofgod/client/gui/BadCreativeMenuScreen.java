2b4d8d5b84e7cde0a54ae9f1516626211bd2ae38
Tfarcenim <44327798+Tfarcenim@users.noreply.github.com>
2021-11-06 14:06:41 -0400

    progress on pocket

--- src/main/java/tfar/thehandofgod/client/gui/BadCreativeMenuScreen.java
+++ src/main/java/tfar/thehandofgod/client/gui/BadCreativeMenuScreen.java
@@ -1,16 +1,45 @@
 package tfar.thehandofgod.client.gui;
 
+import net.minecraft.client.gui.GuiTextField;
 import net.minecraft.client.gui.inventory.GuiContainer;
+import net.minecraft.client.gui.inventory.GuiContainerCreative;
 import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.resources.I18n;
+import net.minecraft.client.util.SearchTreeManager;
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.inventory.IInventory;
+import net.minecraft.item.Item;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.MathHelper;
+import org.lwjgl.input.Keyboard;
+import org.lwjgl.input.Mouse;
 import tfar.thehandofgod.TheHandOfGod;
 import tfar.thehandofgod.menu.BadCreativeMenu;
+
+import java.io.IOException;
+import java.util.Locale;
 
 public class BadCreativeMenuScreen extends GuiContainer {
 
     private final IInventory playerInventory;
     private static final ResourceLocation GUI_TEXTURE = new ResourceLocation(TheHandOfGod.MODID, "textures/gui/heavenly_pocket.png");
+
+    /**
+     * Amount scrolled in Creative mode inventory (0 = top, 1 = bottom)
+     */
+    private float currentScroll;
+    /**
+     * True if the scrollbar is being dragged
+     */
+    private boolean isScrolling;
+    /**
+     * True if the left mouse button was held down last time drawScreen was called.
+     */
+    private boolean wasClicking;
+    private GuiTextField searchField;
+
+    private boolean clearSearch;
+
 
     public BadCreativeMenuScreen(BadCreativeMenu container) {
         super(container);
@@ -20,11 +49,116 @@ public class BadCreativeMenuScreen extends GuiContainer {
     }
 
     @Override
+    public void initGui() {
+        super.initGui();
+        BadCreativeMenu badCreativeMenu = (BadCreativeMenu)this.inventorySlots;
+        this.dragSplittingSlots.clear();
+        badCreativeMenu.itemList.clear();
+        this.buttonList.clear();
+        Keyboard.enableRepeatEvents(true);
+        this.searchField = new GuiTextField(0, this.fontRenderer, this.guiLeft + 82, this.guiTop + 6, 80, this.fontRenderer.FONT_HEIGHT);
+        this.searchField.setMaxStringLength(50);
+        this.searchField.setEnableBackgroundDrawing(false);
+        this.searchField.setTextColor(0xffffff);
+
+        this.searchField.setCanLoseFocus(false);
+        this.searchField.setFocused(true);
+        this.searchField.setText("");
+        //  this.searchField.width = tab.getSearchbarWidth();
+        //   this.searchField.x = this.guiLeft + (82 /*default left*/ + 89 /*default width*/) - this.searchField.width;
+        this.updateSearch();
+
+        this.currentScroll = 0.0F;
+        badCreativeMenu.scrollTo(0.0F);
+    }
+
+    @Override
     public void drawScreen(int mouseX, int mouseY, float partialTicks) {
         drawDefaultBackground();
+
+        boolean flag = Mouse.isButtonDown(0);
+        int i = this.guiLeft;
+        int j = this.guiTop;
+        int k = i + 175;
+        int l = j + 18;
+        int i1 = k + 14;
+        int j1 = l + 112;
+
+        if (!this.wasClicking && flag && mouseX >= k && mouseY >= l && mouseX < i1 && mouseY < j1)
+        {
+            this.isScrolling = this.needsScrollBars();
+        }
+
+        if (!flag)
+        {
+            this.isScrolling = false;
+        }
+
+        this.wasClicking = flag;
+
+        if (this.isScrolling)
+        {
+            this.currentScroll = ((float)(mouseY - l) - 7.5F) / ((float)(j1 - l) - 15.0F);
+            this.currentScroll = MathHelper.clamp(this.currentScroll, 0.0F, 1.0F);
+            ((BadCreativeMenu)this.inventorySlots).scrollTo(this.currentScroll);
+        }
+
         super.drawScreen(mouseX, mouseY, partialTicks);
+
+
         renderHoveredToolTip(mouseX, mouseY);
     }
+
+    @Override
+    public void onGuiClosed() {
+        super.onGuiClosed();
+        Keyboard.enableRepeatEvents(false);
+    }
+
+    /**
+     * Fired when a key is typed (except F11 which toggles full screen). This is the equivalent of
+     * KeyListener.keyTyped(KeyEvent e). Args : character (character on the key), keyCode (lwjgl Keyboard key code)
+     */
+    protected void keyTyped(char typedChar, int keyCode) throws IOException {
+        if (this.clearSearch) {
+            this.clearSearch = false;
+            this.searchField.setText("");
+        }
+
+        if (!this.checkHotbarKeys(keyCode)) {
+            if (this.searchField.textboxKeyTyped(typedChar, keyCode)) {
+                this.updateSearch();
+            } else {
+                super.keyTyped(typedChar, keyCode);
+            }
+        }
+    }
+
+    private void updateSearch() {
+        BadCreativeMenu guicontainercreative$containercreative = (BadCreativeMenu) this.inventorySlots;
+        guicontainercreative$containercreative.itemList.clear();
+
+        if (this.searchField.getText().isEmpty()) {
+            for (Item item : Item.REGISTRY) {
+                item.getSubItems(CreativeTabs.SEARCH, guicontainercreative$containercreative.itemList);
+            }
+        } else {
+            guicontainercreative$containercreative.itemList.addAll(this.mc.getSearchTree(SearchTreeManager.ITEMS).search(this.searchField.getText().toLowerCase(Locale.ROOT)));
+        }
+
+        this.currentScroll = 0.0F;
+        guicontainercreative$containercreative.scrollTo(0.0F);
+    }
+
+
+    /**
+     * returns (if you are not on the inventoryTab) and (the flag isn't set) and (you have more than 1 page of items)
+     */
+    private boolean needsScrollBars()
+    {
+        return ((BadCreativeMenu)this.inventorySlots).canScroll();
+    }
+
 
     @Override
     protected void drawGuiContainerBackgroundLayer(float partialTicks, int mouseX, int mouseY) {
@@ -33,6 +167,7 @@ public class BadCreativeMenuScreen extends GuiContainer {
         int i = (this.width - this.xSize) / 2;
         int j = (this.height - this.ySize) / 2;
         drawTexturedModalRect(i, j, 0, 0, this.xSize, this.ySize);
+        this.searchField.drawTextBox();
     }
 
     @Override
--- src/main/java/tfar/thehandofgod/menu/BadCreativeMenu.java
+++ src/main/java/tfar/thehandofgod/menu/BadCreativeMenu.java
@@ -5,14 +5,33 @@ import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.inventory.Container;
 import net.minecraft.inventory.Slot;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.NonNullList;
+import net.minecraftforge.items.ItemStackHandler;
+import net.minecraftforge.items.SlotItemHandler;
 
 public class BadCreativeMenu extends Container {
 
+    public static final int SLOTS = 54;
+
     public final InventoryPlayer playerInventory;
+
+    public final ItemStackHandler handler = new ItemStackHandler(SLOTS);
+
+    /**
+     * the list of items in this container
+     */
+    public NonNullList<ItemStack> itemList = NonNullList.create();
 
     public BadCreativeMenu(InventoryPlayer playerInventory) {
         this.playerInventory = playerInventory;
-        addPlayerSlots(8,140);
+
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 9; j++) {
+                this.addSlotToContainer(new SlotItemHandler(handler, j + i * 9, j * 18 + 8, i * 18 + 18));
+            }
+        }
+
+        addPlayerSlots(8, 140);
     }
 
     protected void addPlayerSlots(int x, int y) {
@@ -62,4 +81,33 @@ public class BadCreativeMenu extends Container {
     public boolean canInteractWith(EntityPlayer playerIn) {
         return true;
     }
+
+    public boolean canScroll() {
+        return this.itemList.size() > SLOTS;
+    }
+
+    /**
+     * Updates the gui slots ItemStack's based on scroll position.
+     */
+    public void scrollTo(float pos) {
+        int i = (this.itemList.size() + 9 - 1) / 9 - 6;
+        int j = (int) (pos * i + 0.5D);
+
+        if (j < 0) {
+            j = 0;
+        }
+
+        for (int k = 0; k < 5; ++k) {
+            for (int l = 0; l < 9; ++l) {
+                int i1 = l + (k + j) * 9;
+
+                if (i1 >= 0 && i1 < this.itemList.size()) {
+                    handler.setStackInSlot(l + k * 9, this.itemList.get(i1));
+                } else {
+                    handler.setStackInSlot(l + k * 9, ItemStack.EMPTY);
+                }
+            }
+        }
+    }
+
 }
